a20f882a7d9bfb29f5b7a53d0cdf0d227b47aeb3
linobuonvino <michele.giuffre@gmail.com>
2025-03-27 16:36:43 +0100

    corretto mydet

--- mydet.m
+++ mydet.m
@@ -1,16 +1,24 @@
 
-function [d] = mydet(A)
-persistent i;
- if isempty(i)
-  i = 1
- endif
- dim = size(A)
- if length(A)-i+1<=2
-  d = d + (A(dim(end-2),1)*A(2,2))-(A(1,2)*A(2,1))
-  return
- endif
- d = (-1)^(i-1)*A(1,i)
- d+= d * mydet(A(i+1:end,i+1:end))
- i++
+function d = mydet(A)
+  dim = size(A);
+  if dim(2)==1
+    d=A;
+  elseif dim(2) == 2
+    d=A(2,2)*A(1,1)-A(2,1)*A(1,2);
+    %d=det(A);
+  else
+    d=0;
+    for j=1:dim(2)
+      if j==1
+        D=A(2:end,2:end);
+      else
+        B=A(2:end,1:j-1);
+        C=A(2:end,j+1:end);
+        D=[B C];
+      endif
+      %d=mydet(D)
+      d += (-1)^(j-1) * A(1,j) * mydet(D);
+    endfor
+  endif
 end
 
